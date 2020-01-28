6ed5d8cc667219e7714995b09622253b9da96540
BryanMacoy <42852801+BryanMacoy@users.noreply.github.com>
2020-01-28 10:14:23 -0800

    Update ImplementSharpUpdatable.cs

--- ImplementSharpUpdatable.cs
+++ ImplementSharpUpdatable.cs
@@ -1,100 +1,39 @@
 using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
-using System.Net;
+using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
-using System.Xml;
 
-namespace SupportSharpUpdate
+namespace SharpUpDate
 {
-    /// <summary>
-    ///  Provides application update support in C#
-    /// </summary>
-    public class SharpUpdater
+    public interface ImplementSharpUpdate
     {
         ///<summary>
-        ///The LatestUpdate object that holds the encapsulated Update XML data
+        /// The name of your application as you want it displayed on the update form
         /// </summary>
-        private LatestUpdate update;
-
-        /// <summary>
-        /// Holds the program-to-update's info
-        /// </summary>
-        private ImplementSharpUpdatable programInfo;
-
-        /// <summary>
-        /// The dialog to accept an update
-        /// </summary>
-        private AcceptupdateForm acceptUpdateForm;
-
-        /// <summary>
-        /// Thread to find update
-        /// </summary>
-        private BackgroundWorker bgWorker;
-
-        /// <summary>
-        /// Creates a new SharpUpdater Object
-        /// </summary>
-        /// <Param name="applicationName">The name of the application so it can be displayed on dialog boxes to user</Param>
-        /// <param name="appId">A unique Id for the application, same as in update xlm</param>
-        /// <param name="updateXmlLocation">The Url for the program's update.xml</param>
-        public SharpUpdater(ImplementSharpUpdatable programInfo)
-        {
-            this.programInfo = programInfo;
-
-            bgWorker = new BackgroundWorker();
-            bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
-            bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
-        }
-
+        string ApplicationName { get; }
         ///<summary>
-        ///Checks for an update for the program passed.
-        ///If there is an update, a dialog asking to downlaod will appear
+        /// An identified string to use to identify you application in the update.xml
+        /// Should be the same as you appID in the update.xml
         /// </summary>
-        public void DoUpdate()
-        {
-            bgWorker.RunWorkerAsync(Tuple.Create<ImplementSharpUpdatable, LatestUpdate>(this.programInfo, this.update));
-        }
-
+        string ApplicationID { get; }
         ///<summary>
-        ///Checks for/parses update.xml on server
+        /// The current assmebly
         /// </summary>
-        /// <param name="e">Tuple&lt;ImplementSharpUpdatable, LatestUpdate&gt;</param>
-        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            ImplementSharpUpdatable program = (ImplementSharpUpdatable)((Tuple<ImplementSharpUpdatable, LatestUpdate>)e.Argument).Item1;
-            latestUpdate update = (latestUpdate)((Tuple<ImplementSharpUpdatable, LatestUpdate>)e.Argument).Item2;
-
-            /// Check for update on server
-            if (!UpdateXmlExist(program))
-            {
-                e.Cancel = true;
-                return;
-            }
-
-            //Parse update xml
-            e.Result = ParseUpdateXml(program);
-        }
-
-        /// <summary>
-        /// After the background worker is done, prompt to update if there is one
+        Assembly ApplicationAssembly { get; }
+        ///<summary>
+        /// The version of you current application
         /// </summary>
-        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            if (e.Cancelled)
-                return;
-
-            this.update = (LatestUpdate)e.Result;
-
-            if (this.update == null)
-                return;
-
-            if (this.update.IsNewerThan(this.programInfo.ApplicationAssembly.GetName().Version))
-            {
-                // Add dialog here to check if you want to download
-                this.acceptUpdateForm = new AcceptupdateForm(this.programInfo, this.update);
-            }
-        }
+        Version ApplicationVersion { get; }
+        ///<summary>
+        /// The version of you current application
+        /// </summary>
+        Icon ApplicationIcon { get; }
+        ///<summary>
+        /// The version of you current application
+        /// </summary>
+        Uri UpdateXmlLocation { get; }
+        ///<summary>
+        /// The version of you current application
+        /// </summary>\
+        Form Context { get; }
     }
-}
